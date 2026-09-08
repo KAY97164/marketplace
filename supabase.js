df30a1c965c830aab55ca68a3163faeb9f07f58c
KAY97164 <hmm81347@gmail.com>
2026-09-08 20:13:38 +0100

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://mjwxzljypessecdvrajl.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_Or5QEFT-lXW5OF0SysRpRw_6g06Dg-h";
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
